f95666867dec0b713f48a9348a3f4529dceba232
bencallis1 <bencallis1@gmail.com>
2016-06-28 16:39:58 -0600

    added color pallet and the three.js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Ben on 6/28/16.
+ */
+
+// define the color pallet that we will be using
+var Colors = {
+    red:0xf25346,
+    white:0xd8d0d1,
+    brown:0x59332e,
+    pink:0xF5986E,
+    brownDark:0x23190f,
+    blue:0x68c3c0,
+};
