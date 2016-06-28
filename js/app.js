73ed1f33c7f10be3cb1450f4e8b6a8546ec1a7e8
bencallis1 <bencallis1@gmail.com>
2016-06-28 16:46:16 -0600

    created the scene for the plane

--- js/app.js
+++ js/app.js
@@ -11,3 +11,94 @@ var Colors = {
     brownDark:0x23190f,
     blue:0x68c3c0,
 };
+
+
+window.addEventListener('load', init, false);
+
+function init() {
+    // set up the scene, the camera and the renderer
+    createScene();
+
+    // add the lights
+    createLights();
+
+    // add the objects
+    createPlane();
+    createSea();
+    createSky();
+
+    // start a loop that will update the objects' positions
+    // and render the scene on each frame
+    loop();
+}
+
+
+// three.js requires that we have these things
+// A scene: consider this as the stage where every object needs to be added in order to be rendered
+// A camera: in this case we will use a perspective camera, but it could also be an orthographic camera.
+// A renderer that will display all the scene using WebGL.
+// One or more objects to render, in our case, we will create a plane, a sea and a sky (a few clouds)
+// One or more lights: there is also different types of lights available. In this project we will mainly use a hemisphere light for the atmosphere and a directional light for the shadows.
+
+var scene,
+    camera, fieldOfView, aspectRatio, nearPlane, farPlane, HEIGHT, WIDTH,
+    renderer, container;
+
+function createScene() {
+    // Get the width and the height of the screen,
+    // use them to set up the aspect ratio of the camera
+    // and the size of the renderer.
+    HEIGHT = window.innerHeight;
+    WIDTH = window.innerWidth;
+
+    // Create the scene
+    scene = new THREE.Scene();
+
+    // Add a fog effect to the scene; same color as the
+    // background color used in the style sheet
+    scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+
+    // Create the camera
+    aspectRatio = WIDTH / HEIGHT;
+    fieldOfView = 60;
+    nearPlane = 1;
+    farPlane = 10000;
+    camera = new THREE.PerspectiveCamera(
+        fieldOfView,
+        aspectRatio,
+        nearPlane,
+        farPlane
+    );
+
+    // Set the position of the camera
+    camera.position.x = 0;
+    camera.position.z = 200;
+    camera.position.y = 100;
+
+    // Create the renderer
+    renderer = new THREE.WebGLRenderer({
+        // Allow transparency to show the gradient background
+        // we defined in the CSS
+        alpha: true,
+
+        // Activate the anti-aliasing; this is less performant,
+        // but, as our project is low-poly based, it should be fine :)
+        antialias: true
+    });
+
+    // Define the size of the renderer; in this case,
+    // it will fill the entire screen
+    renderer.setSize(WIDTH, HEIGHT);
+
+    // Enable shadow rendering
+    renderer.shadowMap.enabled = true;
+
+    // Add the DOM element of the renderer to the
+    // container we created in the HTML
+    container = document.getElementById('world');
+    container.appendChild(renderer.domElement);
+
+    // Listen to the screen: if the user resizes it
+    // we have to update the camera and the renderer size
+    window.addEventListener('resize', handleWindowResize, false);
+}
